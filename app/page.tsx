1cef5d1ebe85022574e48c33da28ec68a9650d38
Anand Loorthu Raj A <garudaoneinc@gmail.com>
2024-12-13 13:30:47 +0530

    My Expense Tracker - Aplha version 0.0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,132 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [product_name, set_product_name] = useState('');
+  const [price, set_price] = useState('');
+  const [product_category, set_product_category] = useState('');
+  const [payment_method,set_payment_method] = useState('');
+  const [transaction_status,set_transaction_status] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  if (transaction_status === '') {
+    set_transaction_status('Save your transactions ');
+  }
+
+  const inputWidth = transaction_status.length ? `${transaction_status.length + 1}ch` : '1ch'; 
+  
+  const handle_submit = () => {
+    const time_at_transaction = new Date();
+    console.log(product_name,price, product_category,payment_method,time_at_transaction)
+    const transaction_message = 'Your transaction is saved';
+    set_transaction_status(transaction_message); 
+  };
+
+  const handle_input_change = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    if (name === "product_category") {
+      set_product_category(value);
+    } else if (name === "product_name") {
+      set_product_name(value);
+    } else if (name === "price") {
+      set_price(value);
+    } else if (name ==="payment_method") {
+      set_payment_method(value);
+    }
+  };
+
+  return (
+    
+    <main className="flex flex-col items-center px-24 pt-24">
+
+      <div className="w-mix-screen bg-white p-5 rounded-lg shadow-lg mb-4">
+        <div className="status-section flex flex-row">
+          <img className='w-10 bg-black p-2 mr-2' src="./vercel.svg" alt="" />
+          <input className="text-lg font-bold text-purple-400 p-2"
+          style={{ width: inputWidth }}
+          value={transaction_status}
+          readOnly/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+
+      <div className="w-max bg-white p-6 rounded-lg shadow-lg">
+        <div className="form-section">
+          
+          <div>
+            <h1 className="text-4xl font-bold mb-2 text-purple-600">METrack app</h1>
+            <h2 className="text-xl font-bold mb-10 text-purple-400">Your Monthly Expense Tracker</h2>
+          </div>
+        
+          <form action={handle_submit} className='flex flex-col'>
+            
+              <input type='text' placeholder='e.g., Chips, Tea, Petrol'
+              className='flex-auto w-full mb-4 bg-stone-200 text-violet-600 font-bold px-2 py-4 ps-4 pe-4 rounded-xl' 
+              name='product_name'
+              required
+              onChange={handle_input_change} 
+              value={product_name}/>
+
+              <input type='number' placeholder='e.g., 50, 25, 506' pattern='[0-9]*' 
+              className='flex-auto w-full mb-4 bg-stone-200 text-violet-600 font-bold px-2 py-4 ps-4 rounded-xl' 
+              name='price'
+              required
+              onChange={handle_input_change} 
+              value={price}/>
+
+              <select
+                name="product_category"
+                className="flex-auto w-full mb-4 bg-stone-200 text-violet-600 font-bold px-2 py-4 ps-3 pe-3 rounded-xl"
+                onChange={handle_input_change}
+                value={product_category}
+              >
+                <option value="" disabled>Type</option>
+                <option value="f&b">F&B</option>
+                <option value="essentials">Essentials</option>
+                <option value="lifestyle">Lifestyle</option>
+                <option value="miscellaneous">Miscellaneous</option>
+              </select>
+
+              <div className="payment-options flex-auto w-full mb-4 text-violet-600 font-bold grid gap-2 grid-cols-3">
+                <label className='border-2 border-stone-200 rounded-xl p-3'>
+                  <input
+                    className='mr-2 accent-violet-600'
+                    type="radio"
+                    name="payment_method"
+                    value="cash"
+                    onChange={handle_input_change}
+                    checked={payment_method === "cash"}
+                  />
+                  Cash
+                </label>
+                <label className='border-2 border-stone-200 rounded-xl p-3'>
+                  <input
+                    className='mr-2 accent-violet-600'
+                    type="radio"
+                    name="payment_method"
+                    value="upi"
+                    onChange={handle_input_change}
+                    checked={payment_method === "upi"}
+                  />
+                  UPI
+                </label>
+
+                <label className='border-2 border-stone-200 rounded-xl p-3'>
+                  <input
+                    className='mr-2 accent-violet-600'
+                    type="radio"
+                    name="payment_method"
+                    value="cc"
+                    onChange={handle_input_change}
+                    checked={payment_method === "cc"}
+                  />
+                  CC
+                </label>
+              </div>
+
+
+              <button type="submit" className='bg-violet-600 px-6 py-4 rounded-xl font-bold'>Save</button>
+          </form>
+        </div>
+      </div>
+    </main>
+  )
 }
