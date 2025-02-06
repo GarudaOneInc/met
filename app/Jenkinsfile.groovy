525b48401edc5484a65c247f574dc7caf9846779
Anand Loorthu Raj A <garudaoneinc@gmail.com>
2025-02-07 03:53:16 +0530

    Add Jenkinsfile for pipeline

---
+++ app/Jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    environment {
+        IMAGE_NAME = "garudaone/metrackv1"
+    }
+    stages {
+        stage('Clone Repo') {
+            steps {
+                git 'https://github.com/GarudaOneInc/met.git'
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                sh 'docker build -t $IMAGE_NAME .'
+            }
+        }
+        stage('Push to Docker Hub') {
+            steps {
+                withDockerRegistry([credentialsId: 'docker-hub-credentials', url: '']) {
+                    sh 'docker push $IMAGE_NAME'
+                }
+            }
+        }
+        stage('Deploy to Kubernetes') {
+            steps {
+                sh 'kubectl apply -f k8s/deployment.yaml'
+            }
+        }
+    }
+}
